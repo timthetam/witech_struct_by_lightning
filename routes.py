81df197e2bd9a715a26d8d7a11d232afac94dd3c
Tim <40153010+timthetam@users.noreply.github.com>
2019-07-13 16:26:03 +1000

    Add files via upload

--- routes.py
+++ routes.py
@@ -6,24 +6,61 @@ from flask import render_template, request, redirect, url_for, session,  jsonify
 #from src.main import Burger, Wrap
 #import pickle
 
-'''
-def fetch_session_order():
-    # Creates a new cart first if the cart never existed
-    if 'orderID' not in session:
-        session['orderID'] = gourmetSystem.create_order()
-    else:
-        # Check the current cookie is valid
-        try:
-            gourmetSystem.get_order(session['orderID'])
-        except KeyError:
-            session['orderID'] = gourmetSystem.create_order()
-    return gourmetSystem.get_order(session['orderID'])
-'''
+
+
+review = {"eye": 0, "ear": 0, "physical": 0, "content" : "", "location": "", 'locationName' : ""}
+
 
 @app.route("/", methods=["GET", "POST"])
-def menu():
+def home():
     
     return render_template("home.html")
+
+
+@app.route("/addReview", methods=["GET", "POST"])
+def addReview():
+
+    if request.method == "POST":
+        # get form data
+        if request.form.get('visualRating'):
+            review['eye'] = 1
+        if request.form.get("auditoryRating"):
+            review['ear'] = 1
+        if request.form.get("physicalRating"):
+            review['physical'] = 1
+
+        review['content'] = request.form.get("reviewText")
+
+        return redirect(url_for('home'))
+
+    return render_template("review.html");
+
+
+@app.route("/listing", methods=["GET"])
+def listing():
+
+    return render_template("listing.html", review=review);
+
+@app.route("/newPlace", methods=["GET", "POST"])
+def newPlace():
+
+    if request.method == "POST":
+        review['location'] = request.form["location"]
+        review['locationName'] = request.form["locationName"]
+        return redirect(url_for('home'))
+
+    return render_template("newPlace.html", review=review);
+
+
+@app.route("/places", methods=["GET"])
+def places():
+
+    return render_template("place.html", review=review);
+
+@app.route("/places2", methods=["GET"])
+def places2():
+
+    return render_template("place2.html", review=review);
 
 '''
 @app.route("/<int:orderID>/customiseBurger", methods=["GET", "POST"])
